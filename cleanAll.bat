047181f1b4e769d4dde31642e9479d123d64ff80
Jay <jayknobler@gmail.com>
2018-05-27 18:32:35 -0400

    Working directory path fixes in batch files. CleanAll.bat now cleans out CleanQtProjects too. Cleaning the shortcuts had the extensions fixed as well.

--- cleanAll.bat
+++ cleanAll.bat
@@ -8,30 +8,38 @@ echo cleaning all projects contained in the sub-directories
 echo in progress...
 echo.
 echo.
-CleanQtProjects.exe StaticLibs
+CleanQtProjects.exe %~dp0\StaticLibs
 echo.
-CleanQtProjects.exe UjpsMainApp
+CleanQtProjects.exe %~dp0\UjpsMainApp
 echo.
-CleanQtProjects.exe Monitoring0
+CleanQtProjects.exe %~dp0\Monitoring0
 echo.
-CleanQtProjects.exe Monitoring1
+CleanQtProjects.exe %~dp0\Monitoring1
 echo.
-CleanQtProjects.exe Monitoring2
+CleanQtProjects.exe %~dp0\Monitoring2
 echo.
-CleanQtProjects.exe AxesCurves
+CleanQtProjects.exe %~dp0\AxesCurves
 echo.
-CleanQtProjects.exe Profiles
+CleanQtProjects.exe %~dp0\Profiles
 echo.
-CleanQtProjects.exe RemoteControllers
+CleanQtProjects.exe %~dp0\RemoteControllers
 echo.
-CleanQtProjects.exe ControllersPlugins
+CleanQtProjects.exe %~dp0\ControllersPlugins
 echo.
 echo.
 
-del UJPS.Ink
-del Monitoring1.Ink
-del Monitoring2.Ink
-del AxesCurves.Ink
+echo cleaning up shortcuts and CleanQtProjects manually
+del %~dp0\CleanQtProjects\.qmake.stash
+del %~dp0\CleanQtProjects\Makefile
+del %~dp0\CleanQtProjects\Makefile.Debug
+del %~dp0\CleanQtProjects\Makefile.Release
+rmdir /S /Q %~dp0\CleanQtProjects\release
+
+del %~dp0\UJPS.lnk
+del %~dp0\Monitoring0.lnk
+del %~dp0\Monitoring1.lnk
+del %~dp0\Monitoring2.lnk
+del %~dp0\AxesCurves.lnk
 
 pause
 
