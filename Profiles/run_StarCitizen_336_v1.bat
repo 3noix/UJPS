a51c71cd4247849f3ac1d6df85ea49800278e789
3noix <nic.trannois@free.fr>
2018-12-01 20:58:26 +0100

    Minor corrections

--- Profiles/run_StarCitizen_336_v1.bat
+++ Profiles/run_StarCitizen_336_v1.bat
@@ -1,7 +1,7 @@
 echo off
 echo In progress... (it can be long the first time)
 echo.
-%~dp0\..\UjpsMainApp\release\UJPS_console.exe %~dp0\StarCitizen_3.1.4_v5\release\StarCitizen_314_v5.dll 15
+%~dp0\..\UjpsMainApp\release\UJPS_console.exe %~dp0\StarCitizen_3.1.4_v5\release\StarCitizen_336_v1.dll 15
 echo.
 echo press a key to close this window
 pause
