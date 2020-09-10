70913c7ca21a0dabcf2847ea5ac416b1391741f0
3noix <nic.trannois@free.fr>
2020-09-10 15:57:39 +0200

    Minor improvements in JS code of the HTTP remote example

--- examples/Profiles/Basic/ProfileHttpRemote/RESOURCES/gui.js
+++ examples/Profiles/Basic/ProfileHttpRemote/RESOURCES/gui.js
@@ -1,18 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////////
 // INIT AND CONSTANTS /////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-let target = "192.168.1.40:8080"; // to provide from an input inside html?
 let msgDisplay = document.getElementById("msgDisplay");
 let name = document.getElementById("name").innerHTML;
 let iError = 0;
-let sendRequest = true;
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // PLATFORM ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 let pf = navigator.platform;
-let useTouchEvents = (pf == "iPhone");
+let useTouchEvents = (pf == "iPhone" || pf == "iPad" || pf == "Android");
 let useMouseEvents = (pf == "Win32" || !useTouchEvents);
 msgDisplay.innerHTML = pf;
 
@@ -25,12 +23,12 @@ let body = document.getElementById("body");
 
 body.onfullscreenerror = function() {
 	msgDisplay.innerHTML = "full screen error";
-}
+};
 
 window.onorientationchange = function(event) {
 	//msgDisplay.innerHTML = "Orientation is now " + event.target.screen.orientation.angle;
 	msgDisplay.innerHTML = "orientation changed";
-}
+};
 
 let requestFullScreen = () => {
 	body.requestFullscreen();
@@ -41,118 +39,111 @@ let requestFullScreen = () => {
 ///////////////////////////////////////////////////////////////////////////////
 // SLIDERS LABELS /////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-let slider1 = document.getElementById("slider1");
-let slabel1 = document.getElementById("slabel1");
-slabel1.innerHTML = 0;
-slider1.oninput = function() {
-	let f = 0.001 * this.value - 1.0;
-	slabel1.innerHTML = f.toString().substring(0,f<0?6:5);
-};
-
-let slider2 = document.getElementById("slider2");
-let slabel2 = document.getElementById("slabel2");
-slabel2.innerHTML = 0;
-slider2.oninput = function() {
-	let f = 0.001 * this.value - 1.0;
-	slabel2.innerHTML = f.toString().substring(0,f<0?6:5);
-};
+{
+	// slider 1
+	let slider1 = document.getElementById("slider1");
+	let slabel1 = document.getElementById("slabel1");
+	slabel1.innerHTML = 0;
+	slider1.oninput = function() {
+		let f = 0.001 * this.value - 1.0;
+		slabel1.innerHTML = f.toString().substring(0,f<0?6:5);
+	};
+	
+	// slider 2
+	let slider2 = document.getElementById("slider2");
+	let slabel2 = document.getElementById("slabel2");
+	slabel2.innerHTML = 0;
+	slider2.oninput = function() {
+		let f = 0.001 * this.value - 1.0;
+		slabel2.innerHTML = f.toString().substring(0,f<0?6:5);
+	};
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // UJPS ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-let sendButtonMouse = (i,b) => {
+let sendButtonMouse = (iButton,bPressed) => {
 	if (!useMouseEvents) return;
-	let url = "http://" + target + "/button/" + name + "/" + i + "/" + (b ? "1" : "0");
-	if (sendRequest) {
-		let reqhttp = new XMLHttpRequest();
-		reqhttp.onerror = function() {
-			iError++;
-			msgDisplay.innerHTML = "An error occured (#" + iError + ")";
-		}
-		reqhttp.open("GET",url,true);
-		reqhttp.send();
-	}
+	let url = window.location.origin + "/button/" + name + "/" + iButton + "/" + (bPressed ? "1" : "0");
+	let reqhttp = new XMLHttpRequest();
+	reqhttp.onerror = function() {
+		iError++;
+		msgDisplay.innerHTML = "An error occured (#" + iError + ")";
+	};
+	reqhttp.open("GET",url,true);
+	reqhttp.send();
 };
 
-let sendButtonTouch = (i,b) => {
+let sendButtonTouch = (iButton,bPressed) => {
 	if (!useTouchEvents) return;
-	let url = "http://" + target + "/button/" + name + "/" + i + "/" + (b ? "1" : "0");
-	if (sendRequest) {
-		let reqhttp = new XMLHttpRequest();
-		reqhttp.onerror = function() {
-			iError++;
-			msgDisplay.innerHTML = "An error occured (#" + iError + ")";
-		}
-		reqhttp.open("GET",url,true);
-		reqhttp.send();
-	}
+	let url = window.location.origin + "/button/" + name + "/" + iButton + "/" + (bPressed ? "1" : "0");
+	let reqhttp = new XMLHttpRequest();
+	reqhttp.onerror = function() {
+		iError++;
+		msgDisplay.innerHTML = "An error occured (#" + iError + ")";
+	};
+	reqhttp.open("GET",url,true);
+	reqhttp.send();
 };
 
-let sendAxis = (i,f) => {
-	let url = "http://" + target + "/axis/" + name + "/" + i + "/" + f;
-	if (sendRequest) {
-		let reqhttp = new XMLHttpRequest();
-		reqhttp.onerror = function() {
-			iError++;
-			msgDisplay.innerHTML = "An error occured (#" + iError + ")";
-		}
-		reqhttp.open("GET",url,true);
-		reqhttp.send();
-	}
+let sendAxis = (iAxis,value) => {
+	let url = window.location.origin + "/axis/" + name + "/" + iAxis + "/" + value;
+	let reqhttp = new XMLHttpRequest();
+	reqhttp.onerror = function() {
+		iError++;
+		msgDisplay.innerHTML = "An error occured (#" + iError + ")";
+	};
+	reqhttp.open("GET",url,true);
+	reqhttp.send();
 };
 
-let sendPov = (i,f) => {
-	let url = "http://" + target + "/pov/" + name + "/" + i + "/" + f;
-	if (sendRequest) {
-		let reqhttp = new XMLHttpRequest();
-		reqhttp.onerror = function() {
-			iError++;
-			msgDisplay.innerHTML = "An error occured (#" + iError + ")";
-		}
-		reqhttp.open("GET",url,true);
-		reqhttp.send();
-	}
+let sendPov = (iPov,value) => {
+	let url = window.location.origin + "/pov/" + name + "/" + iPov + "/" + value;
+	let reqhttp = new XMLHttpRequest();
+	reqhttp.onerror = function() {
+		iError++;
+		msgDisplay.innerHTML = "An error occured (#" + iError + ")";
+	};
+	reqhttp.open("GET",url,true);
+	reqhttp.send();
 };
 
-// initialisation
-(function() {
-	let ujpsButtons = document.querySelectorAll("button[data-type=button]");
+// initialisation of buttons and axes
+{
+	let ujpsButtons = document.querySelectorAll("button[data-type=button][data-number]");
 	for (let ujpsButton of ujpsButtons) {
-		if (ujpsButton.hasAttribute("data-number")) {
-			let number = parseInt(ujpsButton.getAttribute("data-number"));
-			if (!isNaN(number)) {
-				ujpsButton.addEventListener("mousedown", function() {
-					sendButtonMouse(number,true);
-				});
-				ujpsButton.addEventListener("mouseup", function() {
-					sendButtonMouse(number,false);
-				});
-				ujpsButton.addEventListener("touchstart", function() {
-					sendButtonTouch(number,true);
-				});
-				ujpsButton.addEventListener("touchend", function() {
-					sendButtonTouch(number,false);
-				});
-			}
+		let dataNumber = parseInt(ujpsButton.getAttribute("data-number"));
+		if (!isNaN(dataNumber)) {
+			ujpsButton.addEventListener("mousedown", function() {
+				sendButtonMouse(dataNumber,true);
+			});
+			ujpsButton.addEventListener("mouseup", function() {
+				sendButtonMouse(dataNumber,false);
+			});
+			ujpsButton.addEventListener("touchstart", function() {
+				sendButtonTouch(dataNumber,true);
+			});
+			ujpsButton.addEventListener("touchend", function() {
+				sendButtonTouch(dataNumber,false);
+			});
 		}
 	}
 
-	let ujpsAxes = document.querySelectorAll("input[type=range]");
+	let ujpsAxes = document.querySelectorAll("input[type=range][data-type][data-number]");
 	for (let ujpsAxis of ujpsAxes) {
-		if (ujpsAxis.hasAttribute("data-type") && ujpsAxis.hasAttribute("data-number")) {
-			let dataType = ujpsAxis.getAttribute("data-type");
-			let number = parseInt(ujpsAxis.getAttribute("data-number"));
-			if (dataType == "axis" && !isNaN(number)) {
-				ujpsAxis.addEventListener("input", function() {
-					let f = 0.001 * ujpsAxis.value - 1.0;
-					sendAxis(number,f);
-				});
-				ujpsAxis.setAttribute("min",0);
-				ujpsAxis.setAttribute("max",2000);
-				ujpsAxis.setAttribute("value",1000);
-				sendAxis(number,0);
-			}
+		let dataType = ujpsAxis.getAttribute("data-type");
+		let dataNumber = parseInt(ujpsAxis.getAttribute("data-number"));
+		if (dataType == "axis" && !isNaN(dataNumber)) {
+			ujpsAxis.addEventListener("input", function() {
+				let f = 0.001 * ujpsAxis.value - 1.0;
+				sendAxis(dataNumber,f);
+			});
+			ujpsAxis.setAttribute("min",0);
+			ujpsAxis.setAttribute("max",2000);
+			ujpsAxis.setAttribute("value",1000);
+			sendAxis(dataNumber,0);
 		}
 	}
-})();
+}
+
